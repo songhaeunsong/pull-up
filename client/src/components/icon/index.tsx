e868b0c27a3d1a0a4d61776ef5be6975879e0cbb
antisdun <112616257+antisdun@users.noreply.github.com>
2025-01-16 19:38:33 +0900

    fix: src/assets 삭제, icon 컴포넌트 오타 수정

--- client/src/components/icon/index.tsx
+++ client/src/components/icon/index.tsx
@@ -8,7 +8,7 @@ interface IconProps extends React.SVGProps<SVGSVGElement> {
 const Icon = ({ id, size = 24, ...props }: IconProps) => {
   return (
     <svg width={size} height={size} viewBox="0 0 24 24" {...props}>
-      <use ref={`/assets/icons/_sprite.svg#${id}`} />
+      <use href={`/assets/icons/_sprite.svg#${id}`} />
     </svg>
   );
 };
--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,9 +1,6 @@
-import Icon from '@/components/icon';
-
 const HomePage = () => {
   return (
     <>
-      <Icon id="google" size={48} />
       <h1 className="text-blue-500"> home</h1>
     </>
   );
