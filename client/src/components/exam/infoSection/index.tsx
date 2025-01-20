b5b16d5726ad52923878a8de9465b22e92ee674e
unknown <2020yyoung@gmail.com>
2025-01-21 00:00:01 +0900

    publish: 모의고사 페이지 info 컴포넌트 제작

--- client/src/components/exam/infoSection/index.tsx
+++ client/src/components/exam/infoSection/index.tsx
@@ -1,3 +1,23 @@
-const InfoSection = () => {};
+import Icon from '@/components/common/icon';
+
+interface InfoSectionProps {
+  title: string;
+  icon: string;
+  children: React.ReactNode;
+}
+
+const InfoSection = ({ title, icon, children }: InfoSectionProps) => {
+  return (
+    <div className="flex flex-col gap-3">
+      <div className="flex items-center gap-2">
+        <Icon id={icon} size={28} />
+        <span className="font-semibold text-2xl text-stone-900">{title}</span>
+      </div>
+      <div className="bg-white py-10 rounded-xl border border-primary-200 flex justify-center items-center text-4xl font-semibold">
+        {children}
+      </div>
+    </div>
+  );
+};
 
 export default InfoSection;
--- client/src/pages/exam/solve/index.tsx
+++ client/src/pages/exam/solve/index.tsx
@@ -1,10 +1,26 @@
 import ExamProblemList from '@/components/exam/problemlist';
+import InfoSection from '@/components/exam/infoSection';
+import SubmitButton from '@/components/common/submitButton';
 
 const ExamSolvePage = () => {
   return (
     <div className="flex h-full w-full flex-1 items-center gap-20 bg-Main px-16 py-10">
       <ExamProblemList />
-      <h1>ExamSolvePage</h1>
+      <div className="flex flex-col min-w-[380px]">
+        <InfoSection title="남은 시간" icon="time">
+          <span>00: 24: 32</span>
+        </InfoSection>
+        <InfoSection title="풀이 현황" icon="problem">
+          <div className="grid grid-cols-5 gap-4">
+            {Array.from({ length: 25 }, (_, i) => (
+              <button key={i} className="w-12 h-12 rounded-lg text-lg font-semibold bg-stone-100 text-stone-700">
+                {i + 1}
+              </button>
+            ))}
+          </div>
+        </InfoSection>
+        <SubmitButton text={'제출하기'} />
+      </div>
     </div>
   );
 };
