1d46921d83ece73d90e6afcbc014a6b14293fa48
antis <rkdwldms42@naver.com>
2025-01-21 12:05:18 +0900

    refactor: 주석 추가

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -12,7 +12,8 @@ const HomePage = () => {
     <div className="relative h-full w-full bg-primary-500">
       <div className="absolute left-0 top-0 h-full w-3/4 bg-white" />
 
-      <div className="relative flex h-full w-full items-center justify-center gap-[152px]">
+      <div className="relative flex h-full w-full items-center justify-center gap-[20rem]">
+        {/* 좌측 컨테이너 */}
         <div className="flex flex-col gap-12">
           <div className="flex flex-col gap-6">
             <div className="flex gap-4">
@@ -32,7 +33,8 @@ const HomePage = () => {
           </div>
           <SubmitButton text="알림 받으러 가기" color="secondary" onClick={onClick} />
         </div>
-        <img src="/assets/images/home.png" alt="대문 이미지" className="h-auto w-auto" />
+        {/* 우측 컨테이너 */}
+        <img src="/assets/images/home.png" alt="대문 이미지" className="h-auto w-[500px]" />
       </div>
     </div>
   );
--- client/src/pages/signup/index.tsx
+++ client/src/pages/signup/index.tsx
@@ -1,5 +1,16 @@
 const SignUp = () => {
-  return <div></div>;
+  return (
+    <div className="relative h-full w-full bg-primary-500">
+      <div className="absolute left-0 top-0 h-full w-3/4 bg-white" />
+
+      <div className="relative flex h-full w-full items-center justify-center gap-[20rem]">
+        {/* 좌측 컨테이너 */}
+        <div className="flex flex-col gap-12"></div>
+        {/* 우측 컨테이너 */}
+        <img src="/assets/images/home.png" alt="대문 이미지" className="h-auto w-[500px]" />
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
