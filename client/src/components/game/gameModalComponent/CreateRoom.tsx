6dfb3f086abcb268d9fdc6ce9ce0a6c8a2867a60
songhaeunsong <she3469@gmail.com>
2025-01-23 13:00:21 +0900

    style: 랜덤 매칭, 방 생성, 코드 입력 모달 목업

---
+++ client/src/components/game/gameModalComponent/CreateRoom.tsx
@@ -0,0 +1,36 @@
+import SubjectSelector from '@/components/common/csConditionSelector/subjectSelector';
+import { SUBJECT_OPTIONS } from '@/components/common/csConditionSelector/subjectSelector/SubjectOptions';
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+
+const CreateRoom = () => {
+  const [selectedSubjectIds, setSelectedSubjectIds] = useState<string[]>([]);
+  const isDisabled = selectedSubjectIds.length === 0;
+
+  const handleSubjectClick = (id: string) => {
+    setSelectedSubjectIds((prev) => (prev.includes(id) ? prev.filter((subjectId) => subjectId !== id) : [...prev, id]));
+  };
+
+  return (
+    <div className="flex w-full flex-col gap-8 px-10 py-4">
+      <div className="flex flex-col justify-between gap-3">
+        {SUBJECT_OPTIONS.map((subject) => (
+          <SubjectSelector
+            key={subject.id}
+            id={subject.id}
+            name={subject.name}
+            icon={subject.icon}
+            isSelected={selectedSubjectIds.includes(subject.id)}
+            onClick={handleSubjectClick}
+          />
+        ))}
+      </div>
+      <div className="flex w-full flex-col gap-2">
+        <span className="text-center">링크를 생성하고 친구에게 전달해주세요!</span>
+        <Button disabled={isDisabled}>코드 생성</Button>
+      </div>
+    </div>
+  );
+};
+
+export default CreateRoom;
