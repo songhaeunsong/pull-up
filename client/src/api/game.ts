61a3f0d4521d7afe102b363d1341002cd25e8786
송하은 <she3469@gmail.com>
2025-01-30 13:39:32 +0900

    feat: 게임 관련 api 로직

--- client/src/api/game.ts
+++ client/src/api/game.ts
@@ -1,11 +1,59 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import api from './instance';
 import { WinningRate } from '@/types/chart';
+import { Card, Player, RoomStatus } from '@/types/game';
 
-const getWinningRate = () => api.get<WinningRate>('/game/me/winning-rate');
+interface PostCreateGameResponse extends Player {
+  roomId: string;
+  roomStatus: RoomStatus;
+  player1P: Player;
+}
+
+interface StompJoinGameResponse extends Player {
+  roomId: string;
+  player1P: Player;
+  player2P: Player;
+}
+
+const getWinningRate = async () => {
+  const response = await api.get<WinningRate>('game/me/winning-rate');
+  const data = await response.json();
+
+  return data;
+};
 
 export const useGetWinningRate = () =>
   useQuery({
     queryKey: ['winningRate'],
     queryFn: () => getWinningRate(),
+  });
+
+const postCreateGame = async (memberId: string) => {
+  const response = await api.post<PostCreateGameResponse>('game/room', { json: { memberId } });
+  const data = await response.json();
+  return data;
+};
+
+export const usePostCreateGame = () => {
+  // const queryClient = useQueryClient();
+
+  const { mutateAsync } = useMutation({
+    mutationFn: (memberId: string) => postCreateGame(memberId),
+    onSuccess: () => {},
+  });
+
+  return mutateAsync;
+};
+
+const getProblem = async () => {
+  const response = await api.get<Card[]>('game/problems?limit=15');
+  const data = await response.json();
+  return data;
+};
+
+export const useGetProblem = (roomStatus: RoomStatus) =>
+  useQuery({
+    queryKey: ['problems'],
+    queryFn: () => getProblem(),
+    enabled: roomStatus === 'READY',
   });
