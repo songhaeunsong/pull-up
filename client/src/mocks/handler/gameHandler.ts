f7d3e327e75a3fc0ea505130b5b3d0fa77ec831b
송하은 <she3469@gmail.com>
2025-01-31 19:36:18 +0900

    fix: handler 병합 충돌 해결

---
+++ client/src/mocks/handler/gameHandler.ts
@@ -0,0 +1,48 @@
+import { http, HttpResponse } from 'msw';
+
+const cards = [
+  { cardId: 1, type: 'question', disabled: false, content: 'What is the capital of France?' },
+  { cardId: 1, type: 'answer', disabled: false, content: 'Paris' },
+  { cardId: 2, type: 'question', disabled: false, content: 'What is 2 + 2?' },
+  { cardId: 2, type: 'answer', disabled: false, content: '4' },
+  { cardId: 3, type: 'question', disabled: false, content: 'What is the boiling point of water in Celsius?' },
+  { cardId: 3, type: 'answer', disabled: false, content: '100' },
+  { cardId: 4, type: 'question', disabled: false, content: 'Who wrote "Romeo and Juliet"?' },
+  { cardId: 4, type: 'answer', disabled: false, content: 'William Shakespeare' },
+  { cardId: 5, type: 'question', disabled: false, content: 'What is the chemical symbol for gold?' },
+  { cardId: 5, type: 'answer', disabled: false, content: 'Au' },
+  { cardId: 6, type: 'question', disabled: false, content: 'Which planet is known as the Red Planet?' },
+  { cardId: 6, type: 'answer', disabled: false, content: 'Mars' },
+  { cardId: 7, type: 'question', disabled: false, content: 'How many continents are there on Earth?' },
+  { cardId: 7, type: 'answer', disabled: false, content: '7' },
+  { cardId: 8, type: 'question', disabled: false, content: 'What is the largest ocean on Earth?' },
+  { cardId: 8, type: 'answer', disabled: false, content: 'Pacific Ocean' },
+];
+
+export const gameHandler = [
+  http.post('http://localhost:8080/game/room', async ({ request }) => {
+    const { memberId } = (await request.json()) as { memberId: string };
+
+    if (typeof memberId === 'string') {
+      return HttpResponse.json(
+        {
+          roomId: '1234',
+          roomStatus: 'WAITING',
+          player1P: {
+            memberId: memberId,
+            name: '송하은',
+          },
+        },
+        {
+          status: 200,
+        },
+      );
+    }
+  }),
+
+  http.get('http://localhost:8080/game/problems?limit=15', async () => {
+    return HttpResponse.json(cards, {
+      status: 200,
+    });
+  }),
+];
--- client/src/mocks/handler/index.ts
+++ client/src/mocks/handler/index.ts
@@ -3,5 +3,13 @@ import { examHandler } from './examHandler';
 import { interviewHandler } from './interviewHandler';
 import { problemHandler } from './problemHandler';
 import { commentHandler } from './commentHandler';
+import { gameHandler } from './gameHandler';
 
-export const handlers = [...authHandler, ...interviewHandler, ...commentHandler, ...examHandler, ...problemHandler];
+export const handlers = [
+  ...authHandler,
+  ...interviewHandler,
+  ...commentHandler,
+  ...examHandler,
+  ...problemHandler,
+  ...gameHandler,
+];
