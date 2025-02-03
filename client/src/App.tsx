f5cd22b28d0b3dad2dd3ece0ce6c795e9a3c9727
songhaeunsong <she3469@gmail.com>
2025-02-03 16:34:44 +0900

    chore: App.tsx 오타 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,7 @@ const App = () => {
         pauseOnHover
         transition={Bounce}
       />
-      <RouterProvider router={router} />;
+      <RouterProvider router={router} />
     </>
   );
 };
