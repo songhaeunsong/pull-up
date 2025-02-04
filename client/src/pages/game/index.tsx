17a2e4c7f9fe711f979c08011c59d31687efdde6
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2025-02-04 09:50:59 +0900

    chore: 오타 수정

--- client/src/pages/game/index.tsx
+++ client/src/pages/game/index.tsx
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 import useResponsive from '@/hooks/useResponsive';
+=======
+import useIsMobile from '@/hooks/useIsMobile';
+import GameContainer from './gameContainer/GameContainer';
+>>>>>>> 8b7801e (chore: 오타 수정)
 import MobileGameContainer from './gameContainer/MobileGameContainer';
 import GameContainer from './gameContainer/GameContainer';
 
