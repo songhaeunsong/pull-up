286eadbf8e3daae3b6004d4da5588c6810318c2e
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2025-02-04 12:24:04 +0900

    chore: 병합 충돌 메세지 삭제 (#62)

--- client/src/pages/game/index.tsx
+++ client/src/pages/game/index.tsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import useResponsive from '@/hooks/useResponsive';
-=======
-import useIsMobile from '@/hooks/useIsMobile';
-import GameContainer from './gameContainer/GameContainer';
->>>>>>> 8b7801e (chore: 오타 수정)
+
 import MobileGameContainer from './gameContainer/MobileGameContainer';
 import GameContainer from './gameContainer/GameContainer';
 
