45d18d3839da323b0c9649570451c0f66bfc4328
songhaeunsong <she3469@gmail.com>
2025-02-05 18:53:00 +0900

    feat: 대시보드 모바일 뷰 차트 탭

--- client/src/pages/dashboard/MobileDashboardContainer.tsx
+++ client/src/pages/dashboard/MobileDashboardContainer.tsx
@@ -1,5 +1,70 @@
+import Analysis from '@/components/dashboard/charts/Analysis';
+import Score from '@/components/dashboard/charts/Score';
+import Streak from '@/components/dashboard/charts/Streak';
+import WinningRate from '@/components/dashboard/charts/WinningRate';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import ChartContainer from './ChartContainer';
+
+const ACTIVE_STYLE =
+  'data-[state=active]:border-b-[3px] data-[state=active]:border-primary-500 data-[state=active]:text-primary-500';
+
 const MobileDashboardContainer = () => {
-  return <>모바일 차트</>;
+  return (
+    <Tabs defaultValue="today">
+      <TabsList className="grid w-full grid-cols-5">
+        <TabsTrigger className={ACTIVE_STYLE} value="today">
+          오늘의 문제
+        </TabsTrigger>
+        <TabsTrigger className={ACTIVE_STYLE} value="streak">
+          풀이 현황
+        </TabsTrigger>
+        <TabsTrigger className={ACTIVE_STYLE} value="score">
+          점수 현황
+        </TabsTrigger>
+        <TabsTrigger className={ACTIVE_STYLE} value="analysis">
+          강점 분석
+        </TabsTrigger>
+        <TabsTrigger className={ACTIVE_STYLE} value="winningRate">
+          나의 승률
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="today" className="w-full">
+        <div className="flex min-h-[300px] flex-col rounded-2xl bg-white p-5">
+          <ChartContainer>
+            <div className="flex items-center justify-center">오늘의 문제</div>
+          </ChartContainer>
+        </div>
+      </TabsContent>
+      <TabsContent value="streak">
+        <div className="flex min-h-[300px] flex-col rounded-2xl bg-white p-5">
+          <ChartContainer>
+            <Streak />
+          </ChartContainer>
+        </div>
+      </TabsContent>
+      <TabsContent value="score">
+        <div className="flex min-h-[300px] flex-col rounded-2xl bg-white p-5">
+          <ChartContainer>
+            <Score />
+          </ChartContainer>
+        </div>
+      </TabsContent>
+      <TabsContent value="analysis">
+        <div className="flex min-h-[300px] flex-col rounded-2xl bg-white p-5">
+          <ChartContainer>
+            <Analysis />
+          </ChartContainer>
+        </div>
+      </TabsContent>
+      <TabsContent value="winningRate">
+        <div className="flex min-h-[300px] flex-col rounded-2xl bg-white p-5">
+          <ChartContainer>
+            <WinningRate />
+          </ChartContainer>
+        </div>
+      </TabsContent>
+    </Tabs>
+  );
 };
 
 export default MobileDashboardContainer;
