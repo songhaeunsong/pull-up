ceb41e03ae24dd37b8fd3047bf228353376d6f1f
songhaeunsong <she3469@gmail.com>
2025-02-07 11:17:59 +0900

    feat: 방 생성 코드 복사 기능에 쓰로틀링 적용

--- client/src/components/game/gameModalComponent/waiting/WaitingAfterCreating.tsx
+++ client/src/components/game/gameModalComponent/waiting/WaitingAfterCreating.tsx
@@ -1,13 +1,24 @@
 import { Button } from '@/components/ui/button';
+import { useEffect } from 'react';
 import { toast } from 'react-toastify';
+import { throttle } from 'lodash';
 
 const WaitingAfterCreating = ({ code }: { code: string }) => {
-  const handleClipboard = async () => {
-    await navigator.clipboard.writeText(code);
-    toast.success('코드가 복사되었습니다!', {
-      position: 'bottom-center',
-    });
-  };
+  const handleClipboard = throttle(
+    async () => {
+      await navigator.clipboard.writeText(code);
+      toast.success('코드가 복사되었습니다!', {
+        position: 'bottom-center',
+      });
+    },
+    3000,
+    { leading: true, trailing: false },
+  );
+
+  useEffect(() => {
+    if (code.length) handleClipboard();
+  }, [code]);
+
   return (
     <div className="flex w-full flex-col items-center gap-4">
       <span className="font-bold">코드를 복사해 친구에게 공유해주세요!</span>
