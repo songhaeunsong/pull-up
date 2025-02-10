d05db251c3b82c9cdb6248ececa92cfdeb3f2d51
yiyoung yoon <2020yyoung@gmail.com>
2025-02-10 17:23:54 +0900

    publish: 대시보드 아카이브 컨텐츠  모바일 css 수정

--- client/src/components/dashboard/reviewCard/index.tsx
+++ client/src/components/dashboard/reviewCard/index.tsx
@@ -22,11 +22,11 @@ const ReviewCard = ({ id, title, subtitle, tags, isProblem }: ReviewCardProps) =
   return (
     <Link
       to={isProblem ? `/exam/problem/${id}` : `/exam/${id}/result`}
-      className="flex min-w-[500px] flex-col gap-3 rounded-lg px-3 py-2 shadow-sm"
+      className="flex flex-col gap-3 rounded-lg px-3 py-2 shadow-sm"
     >
       <div className="flex w-full items-start justify-between">
         <div className="flex flex-col items-start">
-          <span className="font-semibold text-stone-800">{title}</span>
+          <span className="break-keep font-semibold text-stone-800">{title}</span>
           <span className="text-sm font-semibold text-stone-400">{subtitle}</span>
         </div>
         <Icon id="list" size={32} />
