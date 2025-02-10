0401ccb32e18b75aa161d2db64b6ebb8f6ca6364
yiyoung yoon <2020yyoung@gmail.com>
2025-02-10 20:42:00 +0900

    feat: 대시보드 사이드바 카드 api 연결

--- client/src/components/dashboard/sidebar/index.tsx
+++ client/src/components/dashboard/sidebar/index.tsx
@@ -2,6 +2,9 @@ import { Subject } from '@/types/member';
 import Card from './card';
 import Profile from './profile';
 import { convertSubject } from '@/utils/convertSubject';
+import { useGetArchivedProblemAll, useGetWrongProblemAll } from '@/api/problem';
+import { useGetExamAll } from '@/api/exam';
+import convertDate from '@/utils/convertDate';
 
 interface SideBarProps {
   image: string;
@@ -11,31 +14,41 @@ interface SideBarProps {
 }
 
 const SideBar = ({ image, name, email, subjects }: SideBarProps) => {
-  // 더미데이터
-  const dummyRecent = {
-    content: '제3회 모의고사',
-    date: '2025.01.12',
-    subjects: ['운영체제', '네트워크', '운영체제'],
-  };
+  const { data: wrongProblems, isError } = useGetWrongProblemAll();
+  const { data: archivedProblems } = useGetArchivedProblemAll();
+  const { data: examAll } = useGetExamAll();
 
-  const dummyWrong = {
-    content: '스택의 주요 특징은 무엇입니까?',
-    subjects: ['자료구조'],
-  };
+  // 헬퍼 함수: 문제 리스트 가공
+  function getProcessedProblemList(
+    problemDtos?: { question: string; subject: string }[],
+    defaultMessage: string = '데이터가 없습니다.',
+    limit: number = 10,
+  ) {
+    return (
+      problemDtos?.slice(0, Math.min(problemDtos.length, limit)).map((item) => ({
+        content: item.question,
+        subjects: Array.isArray(item.subject) ? item.subject : [convertSubject(item.subject)],
+      })) ?? [{ content: defaultMessage, subjects: [] }]
+    );
+  }
 
-  const dummyArchive = {
-    content: '스택의 주요 특징은 무엇입니까?',
-    subjects: ['자료구조'],
-  };
+  // 부모 컴포넌트에서 데이터 가공
+  const recentExamList = examAll?.getExamResponses?.slice(0, 1).map((item) => ({
+    content: item.examName,
+    subjects: convertSubject(item.subjects),
+    date: convertDate(item.date),
+  })) ?? [{ content: '데이터가 없습니다.', subjects: [] }];
+  const wrongProblemList = getProcessedProblemList(wrongProblems?.wrongProblemDtos);
+  const archiveProblemList = getProcessedProblemList(archivedProblems?.bookmarkedProblemDtos);
 
   return (
     <div className="flex flex-row gap-3 rounded-2xl bg-white p-5 shadow-sm sm:w-full sm:gap-6 lg:w-[351px] lg:flex-col">
       <Profile image={image} name={name} email={email} subjects={convertSubject(subjects)} />
       <hr className="hidden border-2 border-stone-200 lg:block" />
       <div className="flex w-full flex-row gap-4 lg:flex-col">
-        <Card link="/dashboard/recent" title="최근에 푼 모의고사" data={dummyRecent} />
-        <Card link="/dashboard/wrong" title="내가 틀린 문제" data={dummyWrong} />
-        <Card link="/dashboard/archive" title="아카이브" data={dummyArchive} />
+        <Card link="/dashboard/recent" title="최근에 푼 모의고사" data={recentExamList[0]} />
+        <Card link="/dashboard/wrong" title="내가 틀린 문제" data={wrongProblemList[0]} />
+        <Card link="/dashboard/archive" title="아카이브" data={archiveProblemList[0]} />
       </div>
     </div>
   );
