66d91a688cfbd8d208116e2cc13f66f06b5c5935
songhaeunsong <she3469@gmail.com>
2025-02-11 19:38:45 +0900

    fix: 매칭 성공 토스트 띄우는 조건 수정

--- client/src/components/game/GameModal.tsx
+++ client/src/components/game/GameModal.tsx
@@ -141,7 +141,7 @@ const GameModals = () => {
   }, [roomStatus]);
 
   useEffect(() => {
-    if (roomStatus === 'PLAYING') {
+    if (roomId && roomStatus === 'PLAYING') {
       if (createRoomTimeoutRef.current) {
         clearTimeout(createRoomTimeoutRef.current);
       }
@@ -162,16 +162,23 @@ const GameModals = () => {
         triggerName="랜덤 매칭"
         triggerColor="primary"
         onOpenChange={(isOpen: boolean) => handleCloseModal(isOpen)}
+        isOutsideClickable={true}
       >
         <WaitingRamdom handleGameState={handleRandomRoom} />
       </Modal>
-      <Modal triggerName="방 생성" triggerColor="primary" onOpenChange={(isOpen: boolean) => handleCloseModal(isOpen)}>
+      <Modal
+        triggerName="방 생성"
+        triggerColor="primary"
+        onOpenChange={(isOpen: boolean) => handleCloseModal(isOpen)}
+        isOutsideClickable={false}
+      >
         {isPlayerReady ? <WaitingAfterCreating /> : <CreateRoom handleGameState={handleCreateRoom} />}
       </Modal>
       <Modal
         triggerName="코드 입력"
         triggerColor="primary"
         onOpenChange={(isOpen: boolean) => handleCloseModal(isOpen)}
+        isOutsideClickable={true}
       >
         {isPlayerReady ? (
           <Waiting text="입장 중..." />
