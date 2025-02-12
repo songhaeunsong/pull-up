54bf397f89aad1b580b96858c6b4d8828d8eb774
antis <rkdwldms42@naver.com>
2025-02-12 17:58:18 +0900

    Fix: isSuccess 추가

--- client/src/pages/redirect/index.tsx
+++ client/src/pages/redirect/index.tsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 
 const RedirectPage = () => {
   const navigate = useNavigate();
-  const { data: auth, isLoading: isAuthLoading } = useAuthInfo();
+  const { data: auth, isLoading: isAuthLoading, isSuccess } = useAuthInfo();
   const { refetch } = useGetMemberInfo();
   const { setMember, setIsSolvedToday, setIsLoggedIn, setInterviewAnswerId } = memberStore();
 
@@ -14,46 +14,50 @@ const RedirectPage = () => {
     const handleRedirect = async () => {
       console.log('리다이렉트');
       console.log('현재 상태:', { isAuthLoading, auth });
-      if (!isAuthLoading && !auth) {
-        console.log('유저 정보 없음');
-        navigate('/signin');
-        return;
-      }
+      if (isSuccess) {
+        if (!isAuthLoading && !auth) {
+          console.log('유저 정보 없음');
+          navigate('/signin');
+          return;
+        }
 
-      if (!isAuthLoading && auth) {
-        console.log('멤버 정보 요청');
-        const memberData = await refetch();
-        console.log('멤버 정보 요청 성공');
+        if (!isAuthLoading && auth) {
+          console.log('멤버 정보 요청');
+          const memberData = await refetch();
+          console.log('멤버 정보 요청 성공');
 
-        if (memberData) {
-          // 미가입시
-          if (!auth.isSignedUp) {
+          if (memberData) {
+            // 미가입시
+            if (!auth.isSignedUp) {
+              setIsLoggedIn(true);
+              navigate('/signup');
+              return;
+            }
+
+            // 관심과목 미선택시
+            if (!memberData.interestSubjects) {
+              setIsLoggedIn(true);
+              navigate('/signup');
+              return;
+            }
+
+            console.log('로그인 완료');
+
+            // 유저 정보 저장
+            setMember(memberData);
             setIsLoggedIn(true);
-            navigate('/signup');
-            return;
+            setIsSolvedToday(auth.isSolvedToday);
+            setInterviewAnswerId(auth.interviewAnswerId);
+            navigate(auth.isSolvedToday ? `/interview/result/${auth.interviewAnswerId}` : '/interview');
           }
-
-          // 관심과목 미선택시
-          if (!memberData.interestSubjects) {
-            setIsLoggedIn(true);
-            navigate('/signup');
-            return;
-          }
-
-          console.log('로그인 완료');
-
-          // 유저 정보 저장
-          setMember(memberData);
-          setIsLoggedIn(true);
-          setIsSolvedToday(auth.isSolvedToday);
-          setInterviewAnswerId(auth.interviewAnswerId);
-          navigate(auth.isSolvedToday ? `/interview/result/${auth.interviewAnswerId}` : '/interview');
         }
       }
     };
 
     handleRedirect();
-  }, [auth, isAuthLoading]);
+  }, [isSuccess]);
+
+  return null;
 };
 
 export default RedirectPage;
