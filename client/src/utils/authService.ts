0d6116d1ddb0b2ed98e66528812137f107908dc8
antis <rkdwldms42@naver.com>
2025-02-13 13:04:00 +0900

    fix: ky error 타입 확장

--- client/src/utils/authService.ts
+++ client/src/utils/authService.ts
@@ -1,6 +1,7 @@
 import { logout, reissue } from '@/api/auth';
 import api from '@/api/instance';
 import { API_RETRY_COUNT } from '@/constants/auth';
+import { ErrorResponseType } from '@/types/response/error';
 import { BeforeRetryHook } from 'ky';
 
 const AUTH_TOKEN_KEY = 'auth_access_token';
@@ -41,7 +42,10 @@ export const setTokenHeader = (request: Request) => {
 // 토큰 재발급
 export const handleRefreshToken: BeforeRetryHook = async ({ error, retryCount }) => {
   console.log('토큰 에러: ', error);
-  if (error.errorMessage === '[ACCESS_TOKEN] 만료된 Token 입니다.') {
+  const customError = error as ErrorResponseType;
+
+  console.log('커스텀 에러: ', customError);
+  if (customError.errorMessage === '[ACCESS_TOKEN] 만료된 Token 입니다.') {
     console.log('토큰 만료');
 
     if (retryCount === API_RETRY_COUNT - 1) {
