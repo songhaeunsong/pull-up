e1d040797aef3502b230214deb461e23c7473ffd
yiyoung yoon <2020yyoung@gmail.com>
2025-02-13 14:33:49 +0900

    refactor: layout suspense 삭제

--- client/src/layouts/index.tsx
+++ client/src/layouts/index.tsx
@@ -2,18 +2,12 @@ import Header from '@/components/common/header/Header';
 import MobileHeader from '@/components/common/header/MobileHeader';
 import { cn } from '@/lib/utils';
 import { memberStore } from '@/stores/memberStore';
-import { Suspense, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Outlet, useLocation, useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
 
 const SM_STYLE = 'mx-auto max-w-[430px]';
 const BASIC_STYLE = 'sm:mx-0 sm:max-w-full w-full h-screen';
-
-const PageFallback = () => (
-  <div className="flex h-full w-full items-center justify-center bg-Main">
-    <span className="text-lg font-semibold">로딩 중...</span>
-  </div>
-);
 
 const MainLayout = () => {
   const location = useLocation();
@@ -37,11 +31,9 @@ const MainLayout = () => {
     <div className={cn(SM_STYLE, BASIC_STYLE)}>
       <Header />
       <MobileHeader />
-      <Suspense fallback={<PageFallback />}>
-        <main className="h-full w-full">
-          <Outlet />
-        </main>
-      </Suspense>
+      <main className="h-full w-full">
+        <Outlet />
+      </main>
     </div>
   );
 };
