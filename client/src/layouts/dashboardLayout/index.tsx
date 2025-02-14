453c9f629815903f14322b75061ad29d51337c01
yiyoung yoon <2020yyoung@gmail.com>
2025-02-14 13:09:00 +0900

    fix: 문제 상세 보기 페이지에서 문제 번호 수정

--- client/src/layouts/dashboardLayout/index.tsx
+++ client/src/layouts/dashboardLayout/index.tsx
@@ -1,11 +1,12 @@
 import { useGetMemberInfo } from '@/api/member';
 import SideBar from '@/components/dashboard/sidebar';
-import MobileTopBar from '@/components/dashboard/sidebar/MobileTopBar';
+//import MobileTopBar from '@/components/dashboard/sidebar/MobileTopBar';
 import useResponsive from '@/hooks/useResponsive';
 import Page404 from '@/pages/404';
 import { Member } from '@/types/member';
-import { useEffect, useState } from 'react';
+import { lazy, Suspense, useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+const MobileTopBar = lazy(() => import('@/components/dashboard/sidebar/MobileTopBar'));
 
 const DashBoardLayout = () => {
   const { isMobile, isTabletMd } = useResponsive();
@@ -25,13 +26,16 @@ const DashBoardLayout = () => {
   return (
     <div className="flex min-h-screen bg-Main pt-[94px] sm:pt-16">
       {isMobile || isTabletMd ? (
-        <div className="flex flex-col gap-5 p-6">
-          <MobileTopBar
-            image={memberData.profileImageUrl}
-            name={memberData.name}
-            email={memberData.email}
-            subjects={memberData.interestSubjects}
-          />
+        <div className="flex flex-col gap-5 p-2 py-4 sm:p-4">
+          <Suspense fallback={<div>Loading...</div>}>
+            <MobileTopBar
+              image={memberData.profileImageUrl}
+              name={memberData.name}
+              email={memberData.email}
+              subjects={memberData.interestSubjects}
+            />
+          </Suspense>
+
           <Outlet />
         </div>
       ) : (
